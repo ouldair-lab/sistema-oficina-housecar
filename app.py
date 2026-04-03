14932f78f22aff4d5b03546b9b1f3a48120d907b
Ouldair Ribeiro <ouldair@gmail.com>
2026-04-03 18:32:41 -0300

    atualização de logo e navegação

--- app.py
+++ app.py
@@ -33,6 +33,13 @@ EMPRESA = {
 
 app = Flask(__name__)
 
+@app.after_request
+def add_header(response):
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
+
 # 🔐 CHAVE SECRETA
 app.secret_key = "housecar_secreta_123"
 
